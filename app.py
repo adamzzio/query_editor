28a92ca9ed31dd11cc36e5095b95e8c3118498dc
Adam Maurizio <120027998+adamzzio@users.noreply.github.com>
2025-04-26 19:33:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ dashboard_page = st.Page("dashboard.py", title="Dashboard", icon=":material/quer
 add_table_page = st.Page("add_table.py", title="Add Table", icon=":material/playlist_add:")
 table_view_page = st.Page("table_view.py", title="Table View", icon=":material/table:")
 query_editor_page = st.Page("query_editor.py", title="Query Editor", icon=":material/edit_note:")
+python_page = st.Page("python_support.py", title='Python Support', icon=":material/terminal:")
 
 # ===== Set Navigation =====
 pg = st.navigation([dashboard_page, add_table_page,
-                    table_view_page, query_editor_page])
+                    table_view_page, query_editor_page,
+                    python_page])
 pg.run()
